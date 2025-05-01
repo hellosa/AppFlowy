828798d4c0cc4d8827214c97a0cb1d9991f93555
hellosa <czbug.cn@gmail.com>
2025-05-01 14:10:27 +0800

    deeplink 能接受 page view 的 url

--- frontend/appflowy_flutter/lib/startup/tasks/appflowy_cloud_task.dart
+++ frontend/appflowy_flutter/lib/startup/tasks/appflowy_cloud_task.dart
@@ -9,6 +9,7 @@ import 'package:appflowy/startup/tasks/deeplink/deeplink_handler.dart';
 import 'package:appflowy/startup/tasks/deeplink/expire_login_deeplink_handler.dart';
 import 'package:appflowy/startup/tasks/deeplink/invitation_deeplink_handler.dart';
 import 'package:appflowy/startup/tasks/deeplink/login_deeplink_handler.dart';
+import 'package:appflowy/startup/tasks/deeplink/page_view_deeplink_handler.dart';
 import 'package:appflowy/startup/tasks/deeplink/payment_deeplink_handler.dart';
 import 'package:appflowy/user/application/auth/auth_error.dart';
 import 'package:appflowy/user/application/user_auth_listener.dart';
@@ -28,7 +29,8 @@ class AppFlowyCloudDeepLink {
       ..register(LoginDeepLinkHandler())
       ..register(PaymentDeepLinkHandler())
       ..register(InvitationDeepLinkHandler())
-      ..register(ExpireLoginDeepLinkHandler());
+      ..register(ExpireLoginDeepLinkHandler())
+      ..register(PageViewDeepLinkHandler());
 
     _deepLinkSubscription = _AppLinkWrapper.instance.listen(
       (Uri? uri) async {
@@ -139,6 +141,17 @@ class AppFlowyCloudDeepLink {
             _completer?.complete(result);
             completer = null;
           }
+        } else if (handler is PageViewDeepLinkHandler) {
+          // Handle page view deep link results
+          result.onFailure((error) {
+            final context = AppGlobals.rootNavKey.currentState?.context;
+            if (context != null) {
+              showToastNotification(
+                message: 'Failed to navigate to page: ${error.msg}',
+                type: ToastificationType.error,
+              );
+            }
+          });
         } else if (handler is ExpireLoginDeepLinkHandler) {
           result.onFailure(
             (error) {
